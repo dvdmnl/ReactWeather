95949bb6b358a3299e0a6da8651d218ff75f4967
David Manela <dudimanela@gmail.com>
2017-02-06 19:12:17 +0200

    Add Foundation & Jquery

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute, browserHistory } from 'react-router'
 import routes from './Routes/Routes'
 
+//Load Foundation
+require('style!css!foundation-sites/dist/css/foundation.min.css')
+$(document).foundation();
+
 ReactDOM.render((
     <Router history={browserHistory} routes={routes}/>
 ), document.getElementById("app") );
