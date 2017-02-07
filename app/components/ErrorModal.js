13c206f5ff176897075f4a5fdd35c2ee17464b51
David Manela <dudimanela@gmail.com>
2017-02-07 13:47:29 +0200

    Fix Errror Modal After Error

--- app/components/ErrorModal.js
+++ app/components/ErrorModal.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server'
 
 class ErrorModal extends React.Component {
     constructor(props) {
@@ -11,13 +13,9 @@ class ErrorModal extends React.Component {
 
 
     componentDidMount () {
-        var modal = new Foundation.Reveal($('#error-modal'));
-        modal.open();
-    }
-
-    render(){
+        console.log(ReactDOMServer, ReactDOM)
         var {title, message} = this.props;
-        return(
+        var modalMarkup = (
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
                 <h4>{title}</h4>
                 <p>{message}</p>
@@ -28,6 +26,17 @@ class ErrorModal extends React.Component {
                 </p>
             </div>
         )
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup))
+        $(ReactDOM.findDOMNode(this)).html($modal);
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();
+    }
+
+    render(){
+        return (
+            <div>
+            </div>
+            )
     }
 }
 ErrorModal.defaultProps = {
